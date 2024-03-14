1e2e7e69513f28d3b79de550d8891724d6750833
silentbugs <d.misdanitis@gmail.com>
2024-03-14 20:55:55 +0200

    refactor widgets common functionality

--- django_google_maps/widgets.py
+++ django_google_maps/widgets.py
@@ -2,23 +2,29 @@ from django.conf import settings
 from django.forms import widgets
 
 
-class GoogleMapsAddressWidget(widgets.TextInput):
+class BaseGoogleMapsAddressWidget(widgets.TextInput):
+    class Media:
+        css = {
+            'all': ['django_google_maps/css/google-maps-admin.css']
+        }
+        js = [
+            'https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js',
+            'https://maps.google.com/maps/api/js?key={}&libraries=places'.format(
+                settings.GOOGLE_MAPS_API_KEY),
+        ]
+
+
+class GoogleMapsAddressWidget(BaseGoogleMapsAddressWidget):
     """a widget that will place a google map right after the #id_address field"""
     template_name = "django_google_maps/widgets/map_widget.html"
 
     class Media:
-        css = {
-            'all': ('django_google_maps/css/google-maps-admin.css', )
-        }
-        js = (
-            'https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js',
-            'https://maps.google.com/maps/api/js?key={}&libraries=places'.format(
-                settings.GOOGLE_MAPS_API_KEY),
+        js = BaseGoogleMapsAddressWidget.Media.js + [
             'django_google_maps/js/google-maps-admin.js',
-        )
+        ]
 
 
-class GoogleMapsAddressInlineWidget(widgets.TextInput):
+class GoogleMapsAddressInlineWidget(BaseGoogleMapsAddressWidget):
     """
     a widget that will place a google map right after the #id_locations-{index}-address
     field and give it unique identifier.
@@ -26,12 +32,6 @@ class GoogleMapsAddressInlineWidget(widgets.TextInput):
     template_name = 'django_google_maps/widgets/map_widget_inline.html'
 
     class Media:
-        css = {
-            'all': ('django_google_maps/css/google-maps-admin.css', )
-        }
-        js = (
-            'https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js',
-            'https://maps.google.com/maps/api/js?key={}&libraries=places'.format(
-                settings.GOOGLE_MAPS_API_KEY),
+        js = BaseGoogleMapsAddressWidget.Media.js + [
             'django_google_maps/js/google-maps-admin-inline.js',
-        )
+        ]
